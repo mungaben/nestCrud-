27b1b1972c79761e8252b47749b0482fb5873cba
mungaben <kamaumungai3742@gmail.com>
2023-10-06 18:30:51 +0300

    build(swagger): :sparkles: initiated don main tscumantation builder i
    created swager module as  doc builder, used its decopration s in api build methods , dtos and schema type
    
    created swager module as  doc builder, used its decopration s in api build methods , dtos and schema type

--- src/users/dto/entities/user.entities.ts
+++ src/users/dto/entities/user.entities.ts
@@ -2,8 +2,13 @@
 // id,name,email
 // mapped one to one to db
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class User {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   email: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,31 @@
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './dto/entities/user.entities';
 import { UsersService } from './users.service';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
   // controller is layer 1 of 3
+
+  @ApiCreatedResponse({
+    type: User,
+    isArray: true,
+    description: 'get all users',
+  })
   @Get()
   getUsers(): User[] {
     return this.usersService.findAll();
   }
   //   get users by id
+  @ApiCreatedResponse({ type: User })
   @Get(':id')
   getUserById(@Param('id') id: string): User {
     return this.usersService.findById(Number(id));
   }
   //   create users
+  @ApiCreatedResponse({ type: User })
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.create(body);
